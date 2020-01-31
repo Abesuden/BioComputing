c2a15ff8889e47721622dcbd724929ecbc957145
Alexander Besuden <43247793+Abesuden@users.noreply.github.com>
2020-01-31 01:19:41 -0500

    Update sattv.py
    Emulate User: Biologist

--- SAT-TruthValues/sattv.py
+++ SAT-TruthValues/sattv.py
@@ -7,6 +7,7 @@ pyrimidines = ["T", "C"]
 nodes = []
 fishingRods = []
 mer = 20
+sumNodes = 0
 
 def createNodes(truthValues):
     if type(truthValues) != list:
@@ -15,16 +16,30 @@ def createNodes(truthValues):
         if type(truthValues[ex]) != str:
             raise Exception("Invalid truthValues() argument " + str(type(truthValues[ex])) + " all arguments must be of <type 'str'>")
     regEx = []
+    truthValueCount = []
     for i in range(len(truthValues)):
         regEx.append(re.split(",", truthValues[i]))
+    for ex in range(len(regEx)):
+        for ez in range(len(regEx[ex])):
+            if ((len(regEx[ex][ez]) != 2) & (len(regEx[ex][ez]) != 3)):
+                raise Exception("Invalid truthValues() argument length of " + str(len(regEx[ex][ez])) + " characters, each argument's length must be of size 2 or 3 characters (i.e. x1 or ~x1)")
+            if ((len(regEx[ex][ez]) == 3) & (regEx[ex][ez][0] != '`') & (regEx[ex][ez][0] != '~')):
+                raise Exception("Invalid truthValues() argument negation " + "(" + str(regEx[ex][ez][0]) + ")" + str(regEx[ex][ez][1]) + str(regEx[ex][ez][2]) + ", the only accepted negation characters are '~' and '`'")
+            if regEx[ex][ez][len(regEx[ex][ez])-1] not in truthValueCount:
+                truthValueCount.append(regEx[ex][ez][len(regEx[ex][ez])-1])
     nodeNumber = len(regEx) * len(regEx[0])
-    print("      -==Starting node creation==-")
+    print("  -==Starting DNA sequence creation==-")
     print(".---------------------------------------.")
     largestNum = 0
     for elem in range(len(regEx)):
         for subElem in range(len(regEx[elem])):
             if (regEx[elem][subElem][len(regEx[elem][subElem])-1] > largestNum):
                 largestNum = int(regEx[elem][subElem][len(regEx[elem][subElem])-1])
+    excep = largestNum
+    for each in range(len(truthValueCount)):
+        excep -= int(each)
+    if (excep != 0):
+        raise Exception("Invalid truthValues() argument format, truth value assignments cannot skip numbers (i.e. \"x1,x2,x4\" is not an accepted format)")
     for i in range(largestNum*2):
         createdNode = _createNode()
         nodes.append(createdNode)
@@ -32,17 +47,19 @@ def createNodes(truthValues):
             print("| x" + str(i+1) + " |" + createdNode + " node created |")
         else:
             print("|~x" + str((i-largestNum)+1) + " |" + createdNode + " node created |")
-    print("|---------------------------------------|")
+    print("|---------------------------------------'-.")
     for i in range(largestNum+1):
         createdNode = _createNode()
         nodes.append(createdNode)
         if (i == 0):
-            print("| Vin|" + createdNode + " node created |")
+            print("| Vin|" + createdNode + " vertex created |")
         elif (i == largestNum):
-            print("|Vout|" + createdNode + " node created |")
+            print("|Vout|" + createdNode + " vertex created |")
         else:
-            print("| V" + str(i) + " |" + createdNode + " node created |")
-    print("|---------------------------------------'----.")
+            print("| V" + str(i) + " |" + createdNode + " vertex created |")
+    print("|-----------------------------------------'--.")
+    global sumNodes
+    sumNodes = largestNum
     nodeLocation = 0
     vertexLocation = largestNum*2
     for i in range(largestNum*4):
@@ -92,6 +109,47 @@ def changeMer(newLength):
 	else:
 		raise Exception("Invalid argument in changeMer(), new mer must be divisible by two.")
 	print("DNA node length has been changed to " + str(mer) + " mers!")
+
+def report():
+    if not nodes:
+        raise Exception("Cannot make a report until nodes have been created.")
+    if not fishingRods:
+        raise Exception("Cannot make a report until nodes have had their connections made.")
+    out = []
+    print("Creating Report...")
+    fl = open("SAT_TruthValue_Report.txt", "w")
+    out.append("Report")
+    out.append("---------")
+    out.append("\n")
+    out.append("\n")
+    for i in range(len(nodes)):
+        if (i < (len(nodes)-sumNodes)):
+            if (i == 0):
+                out.append("Nodes:")
+            out.append("\n")
+            out.append(nodes[i])
+        else:
+            if (i == (len(nodes)-sumNodes)):
+                out.append("\n")
+                out.append("\n")
+                out.append("Vertices:")
+            out.append("\n")
+            out.append(nodes[i])
+    out.append("\n")
+    out.append("\n")
+    out.append("Connectors:")
+    for i in fishingRods:
+        out.append("\n")
+        out.append(i)
+    out.append("\n")
+    out.append("\n")
+    out.append("---END---")
+    out.append("\n")
+    for i in out:
+        fl.write(i)
+    fl.close()
+    print("")
+    print("Report Generated!")
 
 def _createNode():
 	twentyMer = ""
